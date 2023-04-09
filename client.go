fe98725bb3b01d30578eb154ff7ad99d86a21816
Kirill Kamakin <k.kamakin@protonmail.ch>
2023-04-09 19:21:24 +0200

    Listen to updates

--- client.go
+++ client.go
@@ -5,39 +5,51 @@ import (
 	"fmt"
 	"github.com/gotd/td/telegram"
 	"github.com/gotd/td/telegram/auth"
+	"github.com/gotd/td/telegram/updates"
+	updhook "github.com/gotd/td/telegram/updates/hook"
+	"github.com/gotd/td/tg"
 	"go.uber.org/zap"
 	"os"
 	"os/signal"
 )
 
 type Client struct {
-	client *telegram.Client
-	logger *zap.Logger
-	ctx    context.Context
-	cancel context.CancelFunc
+	client         *telegram.Client
+	log            *zap.Logger
+	ctx            context.Context
+	cancel         context.CancelFunc
+	updatesManager *updates.Manager
 }
 
 func NewClient() (*Client, error) {
-	logger, err := zap.NewDevelopment()
+	log, err := zap.NewDevelopment()
 	if err != nil {
 		return nil, fmt.Errorf("failed to create logger: %w", err)
 	}
-	options := telegram.Options{Logger: logger}
+	updatesManager := newUpdatesManager(log)
+	options := telegram.Options{
+		Logger:        log.Named("telegram"),
+		UpdateHandler: updatesManager,
+		Middlewares: []telegram.Middleware{
+			updhook.UpdateHook(updatesManager.Handle),
+		},
+	}
 	client, err := telegram.ClientFromEnvironment(options)
 	if err != nil {
 		return nil, fmt.Errorf("failed to create client: %w", err)
 	}
 	ctx, cancel := signal.NotifyContext(context.Background(), os.Interrupt)
 	return &Client{
-		client: client,
-		logger: logger,
-		ctx:    ctx,
-		cancel: cancel,
+		client:         client,
+		log:            log.Named("client"),
+		ctx:            ctx,
+		cancel:         cancel,
+		updatesManager: updatesManager,
 	}, nil
 }
 
 func (c *Client) Stop() {
-	_ = c.logger.Sync()
+	_ = c.log.Sync()
 	c.cancel()
 }
 
@@ -45,33 +57,42 @@ func (c *Client) Run() error {
 	return c.client.Run(c.ctx, func(ctx context.Context) error {
 		return c.onClientConnected()
 	})
-
 }
 
 func (c *Client) onClientConnected() error {
-	c.logger.Info("Connected")
+	c.log.Info("Connected")
 	if err := c.checkAuth(); err != nil {
 		return fmt.Errorf("failed to check auth: %w", err)
 	}
-	if err := c.printSelf(); err != nil {
+	self, err := c.printSelf()
+	if err != nil {
 		return fmt.Errorf("failed to print self: %w", err)
 	}
-	return nil
+	if err := c.updatesManager.Auth(c.ctx, c.client.API(), self.ID, self.Bot, true); err != nil {
+		return fmt.Errorf("failed to auth updates manager: %w", err)
+	}
+	defer func() {
+		if err := c.updatesManager.Logout(); err != nil {
+			c.log.Error("Failed to stop updates manager", zap.Error(err))
+		}
+	}()
+	<-c.ctx.Done()
+	return c.ctx.Err()
 }
 
-func (c *Client) printSelf() error {
+func (c *Client) printSelf() (*tg.User, error) {
 	self, err := c.client.Self(c.ctx)
 	if err != nil {
-		return fmt.Errorf("failed to get self: %w", err)
+		return nil, fmt.Errorf("failed to get self: %w", err)
 	}
-	c.logger.Sugar().Infow("Authenticated",
+	c.log.Sugar().Infow("Authenticated",
 		"phone", self.Phone,
 		"id", self.ID,
 		"first_name", self.FirstName,
 		"last_name", self.LastName,
 		"username", self.Username,
 	)
-	return nil
+	return self, nil
 }
 
 func (c *Client) checkAuth() error {
@@ -85,7 +106,7 @@ func (c *Client) checkAuth() error {
 		if !ok {
 			return fmt.Errorf("not authorized and PHONE env var is not set")
 		}
-		c.logger.Info("Not authorized, starting auth flow with number", zap.String("phone", phone))
+		c.log.Info("Not authorized, starting auth flow with number", zap.String("phone", phone))
 		flow := auth.NewFlow(
 			termAuth{phone: phone},
 			auth.SendCodeOptions{},
@@ -94,7 +115,19 @@ func (c *Client) checkAuth() error {
 			return fmt.Errorf("failed to run auth flow: %w", err)
 		}
 	} else {
-		c.logger.Info("Already authorized")
+		c.log.Info("Already authorized")
 	}
 	return nil
+}
+
+func newUpdatesManager(log *zap.Logger) *updates.Manager {
+	updateDispatcher := tg.NewUpdateDispatcher()
+	updateDispatcher.OnNewChannelMessage(func(ctx context.Context, e tg.Entities, update *tg.UpdateNewChannelMessage) error {
+		log.Info("New channel message", zap.Any("message", update.Message))
+		return nil
+	})
+	return updates.New(updates.Config{
+		Handler: updateDispatcher,
+		Logger:  log.Named("updates"),
+	})
 }
