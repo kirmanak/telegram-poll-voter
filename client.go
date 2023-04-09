5c26b3f8bccc60ab4e305173abf85567b4a32a9d
Kirill Kamakin <k.kamakin@protonmail.ch>
2023-04-09 20:56:04 +0200

    Implement sending votes

--- client.go
+++ client.go
@@ -63,7 +63,7 @@ func (c *Client) Run() error {
 }
 
 func (c *Client) onClientConnected() error {
-	c.log.Info("Connected")
+	c.log.Debug("Connected")
 	if err := c.checkAuth(); err != nil {
 		return fmt.Errorf("failed to check auth: %w", err)
 	}
@@ -82,12 +82,96 @@ func (c *Client) onClientConnected() error {
 	for {
 		select {
 		case <-c.ctx.Done():
-			c.log.Info("Context done")
+			c.log.Debug("Context done")
 			return c.ctx.Err()
 		case message := <-c.messages_chan:
-			c.log.Info("Got message", zap.Any("message", message))
+			c.onNotEmptyMessage(message)
 		}
 	}
+}
+
+func (c *Client) onNotEmptyMessage(message tg.NotEmptyMessage) {
+	c.log.Debug("Got message", zap.Any("message", message))
+	msg, ok := message.(*tg.Message)
+	if !ok {
+		c.log.Debug("Not a message")
+		return
+	}
+	media, ok := msg.GetMedia()
+	if !ok {
+		c.log.Debug("Not a media")
+		return
+	}
+	poll, ok := media.(*tg.MessageMediaPoll)
+	if !ok {
+		c.log.Debug("Not a poll")
+		return
+	}
+	c.onPollReceived(poll.Poll, message)
+}
+
+func (c *Client) onPollReceived(poll tg.Poll, msg tg.NotEmptyMessage) {
+	c.log.Debug("Got poll", zap.Any("poll", poll))
+	if len(poll.GetAnswers()) != 2 {
+		c.log.Debug("Not a 2 answers poll")
+		return
+	}
+	if poll.GetClosed() {
+		c.log.Debug("Poll is closed")
+		return
+	}
+	if poll.GetMultipleChoice() {
+		c.log.Debug("Poll is multiple choice")
+		return
+	}
+	if !poll.GetPublicVoters() {
+		c.log.Debug("Poll is anonymous")
+		return
+	}
+	if poll.GetQuiz() {
+		c.log.Debug("Poll is a quiz")
+		return
+	}
+	options := [][]byte{poll.GetAnswers()[0].GetOption()}
+	inputPeer, err := getInputPeer(msg.GetPeerID())
+	if err != nil {
+		c.log.Error("Failed to get input peer", zap.Error(err))
+		return
+	}
+	c.log.Sugar().Debugw(
+		"Sending vote",
+		"input_peer", inputPeer,
+		"msg_id", msg.GetID(),
+		"options", options,
+	)
+	if _, err := c.client.API().MessagesSendVote(c.ctx, &tg.MessagesSendVoteRequest{
+		Peer:    inputPeer,
+		MsgID:   msg.GetID(),
+		Options: options,
+	}); err != nil {
+		c.log.Error("Failed to send vote", zap.Error(err))
+	}
+}
+
+func getInputPeer(peerClass tg.PeerClass) (tg.InputPeerClass, error) {
+	var inputPeer tg.InputPeerClass
+	switch peer := peerClass.(type) {
+	case *tg.PeerUser:
+		inputPeer = &tg.InputPeerUser{
+			UserID: peer.GetUserID(),
+		}
+	case *tg.PeerChat:
+		inputPeer = &tg.InputPeerChat{
+			ChatID: peer.GetChatID(),
+		}
+	case *tg.PeerChannel:
+		inputPeer = &tg.InputPeerChannel{
+			ChannelID: peer.GetChannelID(),
+		}
+	default:
+		return nil, fmt.Errorf("unknown peer type: %T", peerClass)
+	}
+	return inputPeer, nil
 }
 
 func (c *Client) printSelf() (*tg.User, error) {
@@ -116,7 +200,7 @@ func (c *Client) checkAuth() error {
 		if !ok {
 			return fmt.Errorf("not authorized and PHONE env var is not set")
 		}
-		c.log.Info("Not authorized, starting auth flow with number", zap.String("phone", phone))
+		c.log.Debug("Not authorized, starting auth flow with number", zap.String("phone", phone))
 		flow := auth.NewFlow(
 			termAuth{phone: phone},
 			auth.SendCodeOptions{},
@@ -125,7 +209,7 @@ func (c *Client) checkAuth() error {
 			return fmt.Errorf("failed to run auth flow: %w", err)
 		}
 	} else {
-		c.log.Info("Already authorized")
+		c.log.Debug("Already authorized")
 	}
 	return nil
 }
@@ -134,10 +218,10 @@ func newUpdatesManager(log *zap.Logger, ch chan tg.NotEmptyMessage) *updates.Man
 	updateDispatcher := tg.NewUpdateDispatcher()
 	updateLogger := log.Named("updateListener")
 	updateDispatcher.OnNewMessage(func(ctx context.Context, e tg.Entities, update *tg.UpdateNewMessage) error {
-		updateLogger.Info("New message", zap.Any("update.message", update.GetMessage()), zap.Any("entities", e))
+		updateLogger.Debug("New message", zap.Any("update.message", update.GetMessage()), zap.Any("entities", e))
 		nonempty, ok := update.Message.AsNotEmpty()
 		if ok {
-			updateLogger.Info("New non-empty message", zap.Any("nonEmpty", nonempty))
+			updateLogger.Debug("New non-empty message", zap.Any("nonEmpty", nonempty))
 			ch <- nonempty
 		}
 		return nil
